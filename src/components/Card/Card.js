8642f3863f0690096235b1921aae1c620aaef533
Bartuğ Kaan  Çelebi <bartugkaan.celebi@outlook.com>
2023-02-07 22:49:40 +0300

    Added : Card.js and Card.css

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,5 +1,24 @@
 import React from "react";
+import "./Card.css";
 
-export default function Card() {
-  return <div>Card</div>;
+export default function Card({ card, handleChoice, flipped, disabled }) {
+  const handleClick = () => {
+    if (!disabled) {
+      handleChoice(card);
+    }
+  };
+
+  return (
+    <div className="card">
+      <div className={flipped ? "flipped" : ""}>
+        <img className="front" src={card.src} alt="card front" />
+        <img
+          className="back"
+          src="/img/cover.png"
+          alt="card back"
+          onClick={handleClick}
+        />
+      </div>
+    </div>
+  );
 }
