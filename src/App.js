4eb24cabc8692a1b6b105fb2df53da23fa4fd467
Bartuğ Kaan  Çelebi <bartugkaan.celebi@outlook.com>
2023-02-07 22:43:42 +0300

    Added : Shuffle function for cards

--- src/App.js
+++ src/App.js
@@ -1,7 +1,36 @@
-import logo from "./logo.svg";
+import { useState } from "react";
 import "./App.css";
 
+const cardImage = [
+  { src: "/Img/helmet-1.png", matched: false },
+  { src: "/Img/potion-1.png", matched: false },
+  { src: "/Img/ring-1.png", matched: false },
+  { src: "/Img/scroll-1.png", matched: false },
+  { src: "/Img/shield-1.png", matched: false },
+  { src: "/Img/sword-1.png", matched: false },
+];
+
+
 function App() {
+  const [cards, setCards] = useState([]);
+  const [turns, setTurns] = useState(0);
+  const [choiceOne, setChoinceOne] = useState(null);
+  const [choiceTwo, setChoiceTwo] = useState(null);
+  const [disabled, setDisabled] = useState(false);
+
+  //Shuffle Cards
+  const shuffleCards = () => {
+    const shuffledCards = [...cardImage, ...cardImage]
+      .sort(() => Math.random - 0.5)
+      .map((card) => ({ ...card, id: Math.random }));
+
+    setChoiceTwo(null);
+    setChoiceTwo(null);
+    setCards(shuffleCards)
+    setTurns(0);
+  }
+
+
   return <div className="App"></div>;
 }
 
